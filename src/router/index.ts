e55c420b53476d23a1e587f19a84b54342b18431
migue <mmoyano16@gmail.com>
2024-11-08 11:07:42 -0300

    feat: add app layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,16 +5,20 @@ import RegisterView from '../views/auth/RegisterView.vue'
 import AdminView from '../views/admin/AdminView.vue'
 import RacesView from '../views/races/RacesView.vue'
 import RaceView from '../views/races/RaceView.vue'
+import AppLayout from '../views/layout/AppLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView, },
     { path: '/login', name: "login", component: LoginView },
     { path: '/register', name: "register", component: RegisterView },
-    { path: "/admin", name: "admin", component: AdminView },
-    { path: "/races", name: "races", component: RacesView },
-    { path: "/races/:raceId", name: "race", component: RaceView }
+    {
+      path: '/', component: AppLayout, children: [
+        { path: "/admin", name: "admin", component: AdminView },
+        { path: "/races", name: "races", component: RacesView },
+        { path: "/races/:raceId", name: "race", component: RaceView }
+      ]
+    },
   ],
 })
 
---
+++ src/views/layout/AppLayout.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1>soy el layout</h1>
+  <RouterView />
+</template>
